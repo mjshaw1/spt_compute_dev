de5899f3f5e2aa573051bc45c8890292b5074aec
Joseph Gutenson <joseph.l.gutenson@usace.army.mil>
2018-05-13 18:07:01 -0500

    Testing mp upload.

--- spt_compute/ecmwf_forecast_process.py
+++ spt_compute/ecmwf_forecast_process.py
@@ -404,8 +404,8 @@ def run_ecmwf_forecast_process(rapid_executable_location,  # path to RAPID execu
                             raise Exception("ERROR: Invalid mp_mode. Valid types are htcondor and multiprocess ...")
 
                 for rapid_input_directory, watershed_job_info in rapid_watershed_jobs.items():
-                    watershed_job_index = watershed_job_info['jobs'][11]
-                    print("\n{0}".format(watershed_job_info['jobs_info'][watershed_job_index]['data_manager']))
+                    #watershed_job_index = watershed_job_info['jobs'][11]
+                    print("\n{0}".format(watershed_job_info))
                     # add sub job list to master job list
                     master_job_info_list = master_job_info_list + watershed_job_info['jobs_info']
                     if mp_mode == "htcondor":
